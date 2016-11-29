bae7c91805718dcd2fa9b938064531e49a70af6b
Kevin Yao <yaoqinwei@gmail.com>
2016-11-29 17:12:39 +0800

    enable back to previous page by back button on toolbar.

--- app/src/main/java/com/pisces/demo/BaseActivity.java
+++ app/src/main/java/com/pisces/demo/BaseActivity.java
@@ -3,6 +3,7 @@ package com.pisces.demo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class BaseActivity extends AppCompatActivity {
 
@@ -15,5 +16,21 @@ public class BaseActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean bool = true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+
+            default:
+                bool = super.onOptionsItemSelected(item);
+                break;
+        }
+
+        return bool;
     }
 }
